5b72bb3e31d23680794a093529095bbd43b4485a
Tony Chen <chen.tony@bcg.com>
2021-04-07 23:33:44 -0700

    Break up App into components and restyle grocery list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,68 +1,39 @@
 import React, { Component } from 'react';
-import BarChart from "./BarChart";
+import GroceryList from "./GroceryList";
+import Result from "./Result"
+import tofuHero from "./assets/tofu-hero.gif";
+import logo from "./assets/logo-text.png";
 import  "regenerator-runtime";
 
 const API_ADDRESS = 'https://api.ghgi.org';
 
-function GroceryListItem(props) {
-    return(
-        <li key={props.name}>
-            {props.name}<button className="close" onClick={props.remove}>x</button>
-        </li>
-    );
-}
-
-function GroceryList(props) {
-    const list = props.groceryList.map((food, index) =>
-        <GroceryListItem key={food} name={food} remove={() => props.remove(index)}/>);
-    return <ul id="groceryList">{list}</ul>
-}
-
 class App extends Component {
 
-    state = {foodQuery:"", groceryList: [], hasSearched: false, impact_msg: "", contributor_msg: "",
-        avg_impact: 0, contributors: [], impacts: []};
+    state = {apiResponse: {}, hasSearched: false};
 
-    updateFoodQuery = event => {
-        /**
-         * Update state of app to store user input as foodQuery
-         */
-        this.setState({foodQuery: event.target.value});
-    }
-
-    handleKeyPress = event => {
-        /**
-         * React to key presses
-         */
-        if (event.key === 'Enter') {
-            if (this.state.foodQuery === "") {
-                this.search();
-            } else {
-                this.addFood();
-            }
-        }
-    }
-
-    search = async () => {
+    search = async (groceryList) => {
         /**
          * Send grocery list to the GHGI API to retrieve median carbon emission
          * Future extension: error handling
          */
 
         // const recipe = this.parseQuery();
+        console.log(groceryList);
+        for (let i = 0; i < groceryList.length; i++) {
+            let food = groceryList[i];
+            if (food["unit"] === "ea") {
+                groceryList[i] = `${food["quantity"]} ${food["ingredient"]}`;
+            } else {
+                groceryList[i] = `${food["quantity"]} ${food["unit"]} of ${food["ingredient"]}`;
+            }
+        }
 
         let json = await fetch(`${API_ADDRESS}/rateCarbon`,
             {method: 'POST',
                 headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({'recipe': this.state.groceryList})}).then(response => response.json());
-
-        console.log("response", json);
-
-        let [avg_impact, contributors, impacts] = this.parseResponse(json);
-        this.setState({contributors: contributors, hasSearched: true});
-        this.setState({impacts: impacts});
-
-        this.createMessage(avg_impact, contributors, impacts);
+                body: JSON.stringify({'recipe': groceryList})}).then(response => response.json()).then(json => this.setState({hasSearched: true, apiResponse: json}));
+        console.log(this.state.apiResponse);
+        console.log("API response received");
     }
 
     // parseQuery() {
@@ -75,105 +46,18 @@ class App extends Component {
     //     return this.state.foodQuery.split(",");
     // }
 
-    // TODO: Distinguish between originally searched food and actual ingredients
-    parseResponse(json) {
-        /**
-         * Parse response from GHGI API to access data points for display
-         * @param   {String} json      Response body in json format
-         * @return  {[Number, Array]}  Requested data points assembled in an array
-         */
-        console.log("Parsing response");
-        const total_impact = json["impact"];
-        let ingredients = [];
-
-        // Find impact of each ingredient and rank them
-        for (let item of json["items"]) {
-            ingredients.push([item["names"][0], item["impact"]]);
-        }
-        ingredients.sort((a, b) => b[1] - a[1]);
-
-        let contributors = [];
-        let impacts = [];
-        for (let item of ingredients) {
-            contributors.push(item[0]);
-            impacts.push(item[1]);
-        }
-        return [total_impact/contributors.length, contributors, impacts];
-    }
-
-    createMessage(avg_impact, contributors, impacts) {
-        /**
-         * Based on carbon emission of the ingredients provided, customize the message displayed to the user
-         * Future extension: take other params like drive_eq or global_pct
-         * @param   {Number} avg_impact        Avg CO2 equivalent emissions across all ingredients in grams
-         * @param   {Array}  contributors  Top contributors to impact among the ingredients
-         */
-        const ingredient_list_1 = ["beef", "lamb", "pork"]
-        const ingredient_list_2 = ["chicken", "cheese", "fish"]
-        // const ingredient_list_3 = ["chickpeas", "lentils", "tofu", "soy beans", "kale"]
-
-        let top_contributor = contributors[0];
-        let recommendation = "";
-        if (ingredient_list_1.includes(top_contributor)) {
-            recommendation = "Consider replacing it with chicken, cheese, or fish as your source of protein, and help save the world.";
-        } else if (ingredient_list_2.includes(top_contributor)) {
-            recommendation = "Consider replacing it with chickpeas, lentils, tofu, or kale as your source of protein, and help save the world.";
-        } else {
-            recommendation = "You are doing great! Thanks for helping save the world."
-        }
-        this.setState({impact_msg: "The average amount of carbon footprint for your food intake is:"});
-        this.setState({avg_impact: avg_impact});
-        this.setState({contributor_msg: `${top_contributor} is the biggest contributor to carbon emission in this list, with an impact of ${impacts[0]}. ${recommendation}`})
-        console.log(this.state.message);
-    }
-
-    addFood = () => {
-        /**
-         * Add user input to shopping list
-         */
-        let groceryList = this.state.groceryList;
-        groceryList.push(this.state.foodQuery);
-
-        this.setState({
-            foodQuery: "",
-            groceryList: groceryList});
-    }
-
-    removeFood = (index) => {
-        /**
-         * Remove an item from shopping list
-         */
-        let groceryList = this.state.groceryList;
-        groceryList.splice(index, 1);
-
-        this.setState({
-            groceryList: groceryList});
-    }
-
     render(){
-        let result = <div id="result">
-            <h6>{this.state.impact_msg}</h6>
-            <h4>{this.state.avg_impact}</h4>
-            <h6>{this.state.contributor_msg}</h6>
-            <BarChart data={this.state.impacts} labels={this.state.contributors} horizontal={true}/>
-        </div>
-
-        return(
+        return (
             <div id="container">
-                <div id="searchBox">
-                    <h2>Sexy Tofu</h2>
-                    <GroceryList groceryList={this.state.groceryList} remove={this.removeFood}/>
-                    <input
-                        id="foodQuery"
-                        onChange={this.updateFoodQuery}
-                        onKeyPress={this.handleKeyPress}
-                        value={this.state.foodQuery}
-                        placeholder='Add to your grocery list'
-                    />
-                    <button onClick={this.addFood}>Add</button>
-                    <button onClick={this.search}>Search</button>
+                <div id="header">
+                    <a href="#">
+                        <img src={logo} alt="" id="logo"/>
+                    </a>
                 </div>
-                {this.state.hasSearched && result}
+                <img src={tofuHero} alt="" id="tofu-hero"/>
+                <h2>Sexy Tofu</h2>
+                <GroceryList search={this.search}/>
+                {this.state.hasSearched && <Result apiResponse={this.state.apiResponse} />}
             </div>
         );
     }
---
+++ frontend/src/GroceryList.js
@@ -0,0 +1,167 @@
+import React, {Component} from "react";
+import  "regenerator-runtime";
+
+const API_ADDRESS = 'https://api.ghgi.org';
+
+export default class GroceryList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {groceryList: [], currentQuery: ""};
+    }
+
+    updateQuery = event => {
+        /**
+         * Update state of GroceryList to store current user input as foodQuery
+         */
+        this.setState({currentQuery: event.target.value});
+    }
+
+    handleKeyPress = event => {
+        /**
+         * React to key presses
+         */
+        if (event.key === 'Enter') {
+            if (this.state.currentQuery === "") {
+                this.props.search(this.state.groceryList);
+            } else {
+                this.addFood();
+            }
+        }
+    }
+
+    addFood = () => {
+        /**
+         * Add user input to grocery list
+         */
+        this.searchFood(this.state.currentQuery);
+
+        this.setState({
+            currentQuery: ""});
+    }
+
+    updateFood = (index, field, newValue) => {
+        /**
+         * Update an item in shopping list
+         */
+        let groceryList = this.state.groceryList;
+        groceryList[index][field] = newValue;
+
+        this.setState({groceryList: groceryList});
+    }
+
+    removeFood = (index) => {
+        /**
+         * Remove an item from shopping list
+         */
+        let groceryList = this.state.groceryList;
+        groceryList.splice(index, 1);
+
+        this.setState({groceryList: groceryList});
+    }
+
+    searchFood = async (food) => {
+        /**
+         * Send food item to the GHGI API to retrieve default quantity
+         * Future extension: error handling
+         */
+
+        let json = await fetch(`${API_ADDRESS}/rateCarbon`,
+            {method: 'POST',
+                //headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({'recipe': [food]})}).then(response => response.json());
+        console.log(json);
+
+        let groceryList = this.state.groceryList;
+        let item = json["items"][0];
+        groceryList.push({"ingredient": item["product"]["alias"], "quantity": item["qtys"][0]["qty"][0], "unit": item["qtys"][0]["unit"][0]});
+        this.setState({groceryList: groceryList})
+    }
+
+    render() {
+        const list = this.state.groceryList.map((food, index) =>
+            <GroceryListItem
+                key={index}
+                ingredient={food["ingredient"]}
+                quantity={food["quantity"]}
+                unit={food["unit"]}
+                update={(field, newValue) => this.updateFood(index, field, newValue)}
+                remove={() => this.removeFood(index)}
+            />)
+        return (
+            <div id="groceryList">
+                <div id="search">
+                    <input
+                        id="searchBox"
+                        onChange={this.updateQuery}
+                        onKeyPress={this.handleKeyPress}
+                        value={this.state.currentQuery}
+                        placeholder='Try "Tofu" or "2 lbs of chicken breast"'
+                    />
+                    <button onClick={this.addFood}>Add</button>
+                    <button onClick={() => this.props.search(this.state.groceryList)}>Search</button>
+                </div>
+                <ColumnNames />
+                {list}
+            </div>
+        );
+    }
+}
+
+function ColumnNames() {
+    return (<div id="columnNames">
+        <span className="ingredientBox">Ingredient</span>
+        <span className="quantityBox">Quantity</span>
+        <span className="unitBox">Unit</span>
+    </div>)
+}
+
+class GroceryListItem extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {dish: props.dish, ingredient: props.ingredient, quantity: props.quantity, unit: props.unit};
+    }
+
+    handleChange = event => {
+        if (event.target.id.includes("_ingredient")) {
+            this.setState({ingredient: event.target.value});
+            this.props.update("ingredient", event.target.value);
+        }
+        else if (event.target.id.includes("_quantity")) {
+            this.setState({quantity: event.target.value});
+            this.props.update("quantity", event.target.value);
+        } else {
+            this.setState({unit: event.target.value});
+            this.props.update("unit", event.target.value);
+        }
+    }
+
+    render() {
+        return (
+            <div className="groceryListItem">
+                <input
+                    className="ingredientBox"
+                    id={`${this.state.ingredient}_ingredient`}
+                    onChange={this.handleChange}
+                    // onKeyPress={this.handleKeyPress}
+                    value={this.state.ingredient}
+                />
+                <input
+                    className="quantityBox"
+                    id={`${this.state.name}_quantity`}
+                    type={'number'}
+                    onChange={this.handleChange}
+                    // onKeyPress={this.handleKeyPress}
+                    value={this.state.quantity}
+                />
+                <input
+                    className="unitBox"
+                    id={`${this.state.name}_unit`}
+                    onChange={this.handleChange}
+                    // onKeyPress={this.handleKeyPress}
+                    value={this.state.unit}
+                />
+                <button className="close" onClick={this.props.remove}>x</button>
+            </div>
+        );
+    }
+}
