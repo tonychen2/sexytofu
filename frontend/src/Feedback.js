f44fc9890b7057ede6a701e9e21e89a180fdcb3b
QueenChristina <qinkristina@gmail.com>
2021-08-11 14:11:28 -0700

    Move image icon to right position

--- frontend/src/Feedback.js
+++ frontend/src/Feedback.js
@@ -17,7 +17,8 @@ const styles = {
         }
     },
     button: {
-        paddingLeft: '10px',
+        // paddingLeft: '12px',
+        padding: '6px 4px 6px 12px',
         textTransform: "none",
         color: '#53575B',
         backgroundColor: '#F9F7F7',
@@ -67,10 +68,10 @@ class Feedback extends Component {
         return (
             <Grid container justify={"flex-end"} className={this.props.classes.root}>
                 <Button variant="contained" className={this.props.classes.button} onClick={() => {this.openLink(this.props.link)}}> 
-                    <img src={tofuThx} alt="Tofu Thanks" id="tofu-thanks" style={{height: '25px', marginRight: '6px'}}/>
                     <Typography variant="body2">
                         Share your thoughts!
                     </Typography> 
+                    <img src={tofuThx} alt="Tofu Thanks" id="tofu-thanks" style={{height: '25px', marginLeft: '6px'}}/>
                 </Button>
             </Grid>
         )
