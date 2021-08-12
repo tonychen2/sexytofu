6636dede26ec01d35c4b8fcead3536771d7fc820
QueenChristina <qinkristina@gmail.com>
2021-08-12 14:23:21 -0700

    Change grid to box component for feedback button

--- frontend/src/Feedback.js
+++ frontend/src/Feedback.js
@@ -3,12 +3,15 @@ import React, {useState, Component} from "react";
 import tofuThx from "./assets/summary_graphics/tofu_thanks.png";
 
 import {Button} from '@material-ui/core';
-import {Grid, Typography} from '@material-ui/core';
+import {Box, Typography} from '@material-ui/core';
 import {withStyles} from "@material-ui/core";
 
 
 const styles = {
     root: {
+        width:'100%', 
+        display: 'inline-flex', 
+        justifyContent: 'flex-end',
         '& .MuiButton-contained:hover': {
             // Button hover style
             color: '#2e3032',
@@ -66,14 +69,14 @@ class Feedback extends Component {
     
     render() {
         return (
-            <Grid container justify={"flex-end"} className={this.props.classes.root}>
+            <Box className={this.props.classes.root}>
                 <Button variant="contained" className={this.props.classes.button} onClick={() => {this.openLink(this.props.link)}}> 
                     <Typography variant="body2">
                         Share your thoughts!
                     </Typography> 
                     <img src={tofuThx} alt="Tofu Thanks" id="tofu-thanks" style={{height: '25px', marginLeft: '6px'}}/>
                 </Button>
-            </Grid>
+            </Box>
         )
     }
 }
