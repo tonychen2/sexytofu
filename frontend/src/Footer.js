9f659fece767f7db585458f232a9581cac4c3d61
QueenChristina <qinkristina@gmail.com>
2021-08-28 19:33:04 -0700

    Update and revise privacy policy

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -122,7 +122,7 @@ class PolicyPopUp extends Component {
     }
 
     render() {
-        const LAST_UPDATED_DATE = "August 26 2021"
+        const LAST_UPDATED_DATE = "August 28 2021"
         return (
             <Dialog 
                 open={this.props.isOpen} 
@@ -151,47 +151,81 @@ class PolicyPopUp extends Component {
                         <Typography component={'span'} variant="body2" display="block" align="left" style={{marginBottom: '1rem'}}>
                             {/* Google Analytics TOS https://marketingplatform.google.com/about/analytics/terms/us/ */}
                             {/* Text form https://www.termsfeed.com/public/uploads/2019/04/privacy-policy-template.pdf */}
-                            <b>Sexy Tofu</b> operates <a href="https://www.sexytofu.org/">https://www.sexytofu.org/</a> (the&nbsp;
-                            "Site"). This page informs you of our policies regarding the collection, use and disclosure of&nbsp;
-                            Personal Information we receive from users of the Site.&nbsp;
-                            We use your Personal Information only for providing and improving the Site. By using the Site, you&nbsp;
-                            agree to the collection and use of information in accordance with this policy.&nbsp;
+                            {/* What's in a privacy policy: https://www.nolo.com/legal-encyclopedia/what-to-include-in-your-website-s-privacy-policy.html */}
+                            <b>Sexy Tofu</b> operates <a href="https://www.sexytofu.org/">https://www.sexytofu.org/</a> (the "Site").
+                            This privacy policy informs you of our policies regarding the collection, use and disclosure of
+                            personal information we receive from users of the Site.
+                            We use your personal information only for providing and improving the Site. By using the Site, you
+                            agree to the collection and use of information in accordance with this policy.
                         </Typography>
                         <Typography component={'span'} variant="h3" display="block" align="left" style={{color: "#322737", marginBottom: '1rem'}}>
                             Cookies
                         </Typography>
                         <Typography component={'span'} variant="body2" display="block" align="left" style={{marginBottom: '1rem'}}>
-                            Cookies are files with small amount of data, which may include an anonymous unique identifier.&nbsp;
-                            Cookies are sent to your browser from a web site and stored on your computer's hard drive.&nbsp;
-                            Like many sites, we use "cookies" to collect information. You can instruct your browser to refuse all&nbsp;
-                            cookies or to indicate when a cookie is being sent.&nbsp;
+                            Cookies are small files of data, which may include an anonymous unique identifier.
+                            Cookies are sent to your browser from a web site and stored on your computer's hard drive.
+                            Like many sites, we use "cookies" to collect information about your activities
+                            in our services, which may include:
+                            <ul>
+                                <li> Foods you search for</li>
+                                <li> Recommendations you apply</li>
+                                <li> Number of people and length of time your grocery list is for </li>
+                            </ul>
+
                             We use cookies from third-party partners, such as&nbsp;
-                            <a href="www.google.com/policies/privacy/partners/" target="_blank" rel="noopener noreferrer">Google Analytics</a>,&nbsp;
+                            <a href="https://www.google.com/policies/privacy/partners/" target="_blank" rel="noopener noreferrer">Google Analytics</a>,&nbsp;
                             {/* TODO: disclose ALL information collected! be specific. How do you share info? How do you use it? what do you use? */}
-                            to track how you use this website so we can improve this website in the future.&nbsp;
-                            However, if you do not accept cookies, you may not be able to use some portions of our Site.&nbsp;
+                            to track how you use this website so we may improve this website in the future.
+                            You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent.
+                            The amount of information provided by you is voluntary.
+                            However, if you do not accept cookies, you might not be able to use some parts of our Site.
+                        </Typography>
+                        <Typography component={'span'} variant="h3" display="block" align="left" style={{color: "#322737", marginBottom: '1rem'}}>
+                            Your Data
+                        </Typography>
+                        <Typography component={'span'} variant="body2" display="block" align="left" style={{marginBottom: '1rem'}}>
+                            Please note <b>Sexy Tofu</b> takes privacy seriously. Data collected is used for internal purposes only.
+                            We do not sell or rent information about you.
+                            We do not share or disclose your personal information and data to
+                            third parties without your consent, except as explained in this Privacy Policy.
+                        </Typography>
+                        <Typography component={'span'} variant="h3" display="block" align="left" style={{color: "#322737", marginBottom: '1rem'}}>
+                            Compliance with Laws
+                        </Typography>
+                        <Typography component={'span'} variant="body2" display="block" align="left" style={{marginBottom: '1rem'}}>
+                            <b>Sexy Tofu</b> cooperates with the law and government officials to comply with the law. We may disclose
+                            information about you if we deem it reasonably necessary to:
+                            <ol type="a">
+                                <li> Satisfy relevant laws or regulations</li>
+                                <li> Enforce Terms of Use, including investigations of potential violations</li>
+                                {/* TODO: a Terms of Use, and link here */}
+                                <li> Detect, prevent, and address fraud, security, or technical issues</li>
+                                <li> Protect the rights, safety, and property of this company, its users, or the public</li>
+                            </ol>
+                            as required or permitted by law.
+                            Your data is subject to the laws of the United States of America regardless of where the data originates.
                         </Typography>
                         <Typography component={'span'} variant="h3" display="block" align="left" style={{color: "#322737", marginBottom: '1rem'}}>
                             Security
                         </Typography>
                         <Typography component={'span'} variant="body2" display="block" align="left" style={{marginBottom: '1rem'}}>
-                            The security of your Personal Information is important to us, but remember that no method of transmission over the Internet,&nbsp;
-                            or method of electronic storage, is 100% secure.&nbsp;
-                            While we strive to use commercially acceptable means to protect your Personal Information,&nbsp;
-                            we cannot guarantee its absolute security.&nbsp;
+                            The security of your personal information is important to us, but remember that no method of transmission over the internet,
+                            or method of electronic storage, is always completely secure.
+                            While we strive to use commercially acceptable means to protect your Personal Information,
+                            we cannot guarantee its absolute security.
                         </Typography>
                         <Typography component={'span'} variant="h3" display="block" align="left" style={{color: "#322737", marginBottom: '1rem'}}>
                             Changes to this privacy policy
                         </Typography>
                         <Typography component={'span'} variant="body2" display="block" align="left" style={{marginBottom: '1rem'}}>
-                            This Privacy Policy is effective as of <b>{LAST_UPDATED_DATE}</b> and will remain in effect&nbsp;
-                            except with respect to any changes in its provisions in the future,&nbsp;
-                            which will be in effect immediately after being posted on this page.&nbsp;
-                            We reserve the right to update or change our Privacy Policy at any time and&nbsp; 
-                            you should check this Privacy Policy periodically. Your continued use of the Service&nbsp; 
-                            after we post any modifications to the Privacy Policy on this&nbsp;
-                            page will constitute your acknowledgment of the modifications and your consent&nbsp; 
-                            to abide and be bound by the modified Privacy Policy.&nbsp;
+                            This Privacy Policy is effective as of <b>{LAST_UPDATED_DATE}</b> and will remain in effect
+                            except with respect to any changes in its provisions in the future,
+                            which will be in effect immediately after being posted on this page.
+                            We reserve the right to update or change our Privacy Policy at any time and
+                            you should check this Privacy Policy periodically. Your continued use of the Service
+                            after we post any modifications to the Privacy Policy on this
+                            page will constitute your acknowledgment of the modifications and your consent
+                            to abide and be bound by the modified Privacy Policy.
                         </Typography>
                         <Typography component={'span'} variant="h3" display="block" align="left" style={{color: "#322737", marginBottom: '1rem'}}>
                             Contact us
