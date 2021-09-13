35585b9d32ba5ae0bcdc9bb3072285940d9fa6ca
Tony <chensiyu@chensiyu.net>
2021-09-13 16:13:32 -0700

    Update Footer.js
    Formatting and naming changes

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -65,17 +65,17 @@ class Footer extends Component {
      * @param   {String}    props.facebook               Link to open facebook social media.
      * @param   {String}    props.tiktok                 Link to open tiktoksocial media.
      */
-     constructor(props) {
+    constructor(props) {
         super(props);
-        this.state = {isOpen: false};
+        this.state = {popupOpen: false};
     }
 
     openPopUp = () => {
-        this.setState({isOpen: true});
+        this.setState({popupOpen: true});
     }
 
     closePopUp = () => {
-        this.setState({isOpen: false});
+        this.setState({popupOpen: false});
     };
 
     openLink = (link) => {     
@@ -88,21 +88,21 @@ class Footer extends Component {
             <Grid container className={this.props.classes.root} justify="flex-end">
                 <Grid item xs={12} sm={12}>
                     <Typography className={this.props.classes.policy} onClick={this.openPopUp}>Privacy Policy</Typography>
-                    <PolicyPopUp isOpen={this.state.isOpen} onClose={this.closePopUp} classes={this.props.classes} />
+                    <PolicyPopUp isOpen={this.state.popupOpen} onClose={this.closePopUp} classes={this.props.classes} />
                     <Typography className={this.props.classes.text}>Copyright © 2021 SexyTofu</Typography>
                 </Grid>
                 <Grid item xs={12} sm={12}>
-                <div id="socials">
-                    <button variant="contained" className={this.props.classes.button} onClick={() => {this.openLink(this.props.instagram)}}> 
-                        <InstagramIcon />
-                    </button>
-                    <button variant="contained" className={this.props.classes.button} onClick={() => {this.openLink(this.props.facebook)}}> 
-                        <FacebookIcon />
-                    </button>
-                    <button variant="contained" className={this.props.classes.button} onClick={() => {this.openLink(this.props.tiktok)}}> 
-                        <img src={TikTokIcon} alt={"TikTok"}></img>
-                    </button>
-                </div>
+                    <div id="socials">
+                        <button variant="contained" className={this.props.classes.button} onClick={() => {this.openLink(this.props.instagram)}}> 
+                            <InstagramIcon />
+                        </button>
+                        <button variant="contained" className={this.props.classes.button} onClick={() => {this.openLink(this.props.facebook)}}> 
+                            <FacebookIcon />
+                        </button>
+                        <button variant="contained" className={this.props.classes.button} onClick={() => {this.openLink(this.props.tiktok)}}> 
+                            <img src={TikTokIcon} alt={"TikTok"}></img>
+                        </button>
+                    </div>
                 </Grid>
             </Grid>
         )
@@ -141,100 +141,98 @@ class PolicyPopUp extends Component {
                 </DialogActions>
                 <DialogTitle id="dialogTitle" className={this.props.classes.dialogTitle} disableTypography>
                     <Typography variant="h3" style={{color: "#322737"}}> 
-                    Privacy Policy
+                        Privacy Policy
                     </Typography>   
                 </DialogTitle>
                 <DialogContent className={this.props.classes.dialogContent}>
-                    {/* <DialogContentText> */}
-                        <Typography component={'span'} variant="body2" display="block" align="left" style={{marginBottom: '1rem'}}>
+                    <Typography component={'span'} variant="body2" display="block" align="left" style={{marginBottom: '1rem'}}>
                         Last updated: <b>{LAST_UPDATED_DATE}</b>
-                        </Typography>
-                        <Typography component={'span'} variant="body2" display="block" align="left" style={{marginBottom: '1rem'}}>
-                            {/* Google Analytics TOS https://marketingplatform.google.com/about/analytics/terms/us/ */}
-                            {/* Text form https://www.termsfeed.com/public/uploads/2019/04/privacy-policy-template.pdf */}
-                            {/* What's in a privacy policy: https://www.nolo.com/legal-encyclopedia/what-to-include-in-your-website-s-privacy-policy.html */}
-                            <b>Sexy Tofu</b> operates <a href="https://www.sexytofu.org/">https://www.sexytofu.org/</a> (the "Site").
-                            This privacy policy informs you of our policies regarding the collection, use and disclosure of
-                            personal information we receive from users of the Site.
-                            We use your personal information only for providing and improving the Site. By using the Site, you
-                            agree to the collection and use of information in accordance with this policy.
-                        </Typography>
-                        <Typography component={'span'} variant="h3" display="block" align="left" style={{color: "#322737", marginBottom: '1rem'}}>
-                            Cookies
-                        </Typography>
-                        <Typography component={'span'} variant="body2" display="block" align="left" style={{marginBottom: '1rem'}}>
-                            Cookies are small files of data, which may include an anonymous unique identifier.
-                            Cookies are sent to your browser from a web site and stored on your computer's hard drive.
-                            Like many sites, we use "cookies" to collect information about your activities
-                            in our services, which may include:
-                            <ul>
-                                <li> Foods you search for</li>
-                                <li> Recommendations you apply</li>
-                                <li> Number of people and length of time your grocery list is for </li>
-                            </ul>
-                            We use cookies from third-party partners, such as&nbsp;
-                            <a href="https://www.google.com/policies/privacy/partners/" target="_blank" rel="noopener noreferrer">Google Analytics</a>,&nbsp;
-                            {/* TODO: disclose ALL information collected! be specific. How do you share info? How do you use it? what do you use? */}
-                            to track how you use this website so we may improve this website in the future.
-                            You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent.
-                            The amount of information provided by you is voluntary.
-                            However, if you do not accept cookies, you might not be able to use some parts of our Site.
-                        </Typography>
-                        <Typography component={'span'} variant="h3" display="block" align="left" style={{color: "#322737", marginBottom: '1rem'}}>
-                            Your Data
-                        </Typography>
-                        <Typography component={'span'} variant="body2" display="block" align="left" style={{marginBottom: '1rem'}}>
-                            Please note <b>Sexy Tofu</b> takes privacy seriously. Data collected is used for internal purposes only.
-                            We do not sell or rent information about you.
-                            We do not share or disclose your personal information and data to
-                            third parties without your consent, except as explained in this Privacy Policy.
-                        </Typography>
-                        <Typography component={'span'} variant="h3" display="block" align="left" style={{color: "#322737", marginBottom: '1rem'}}>
-                            Compliance with Laws
-                        </Typography>
-                        <Typography component={'span'} variant="body2" display="block" align="left" style={{marginBottom: '1rem'}}>
-                            <b>Sexy Tofu</b> cooperates with the law and government officials to comply with the law. We may disclose
-                            information about you if we deem it reasonably necessary to:
-                            <ol type="a">
-                                <li> Satisfy relevant laws or regulations</li>
-                                <li> Enforce Terms of Use, including investigations of potential violations</li>
-                                {/* TODO: a Terms of Use, and link here */}
-                                <li> Detect, prevent, and address fraud, security, or technical issues</li>
-                                <li> Protect the rights, safety, and property of this company, its users, or the public</li>
-                            </ol>
-                            as required or permitted by law.
-                            Your data is subject to the laws of the United States of America regardless of where the data originates.
-                        </Typography>
-                        <Typography component={'span'} variant="h3" display="block" align="left" style={{color: "#322737", marginBottom: '1rem'}}>
-                            Security
-                        </Typography>
-                        <Typography component={'span'} variant="body2" display="block" align="left" style={{marginBottom: '1rem'}}>
-                            The security of your personal information is important to us, but remember that no method of transmission over the internet,
-                            or method of electronic storage, is always completely secure.
-                            While we strive to use commercially acceptable means to protect your Personal Information,
-                            we cannot guarantee its absolute security.
-                        </Typography>
-                        <Typography component={'span'} variant="h3" display="block" align="left" style={{color: "#322737", marginBottom: '1rem'}}>
-                            Changes to this privacy policy
-                        </Typography>
-                        <Typography component={'span'} variant="body2" display="block" align="left" style={{marginBottom: '1rem'}}>
-                            This Privacy Policy is effective as of <b>{LAST_UPDATED_DATE}</b> and will remain in effect
-                            except with respect to any changes in its provisions in the future,
-                            which will be in effect immediately after being posted on this page.
-                            We reserve the right to update or change our Privacy Policy at any time and
-                            you should check this Privacy Policy periodically. Your continued use of the Service
-                            after we post any modifications to the Privacy Policy on this
-                            page will constitute your acknowledgment of the modifications and your consent
-                            to abide and be bound by the modified Privacy Policy.
-                        </Typography>
-                        <Typography component={'span'} variant="h3" display="block" align="left" style={{color: "#322737", marginBottom: '1rem'}}>
-                            Contact us
-                        </Typography>
-                        <Typography component={'span'} variant="body2" display="block" align="left" style={{marginBottom: '1rem'}}>
-                            If you have any questions about this Privacy Policy, please&nbsp;
-                            <a href="mailto: sexytofu.info@gmail.com">contact us</a>.
-                        </Typography>
-                    {/* </DialogContentText> */}
+                    </Typography>
+                    <Typography component={'span'} variant="body2" display="block" align="left" style={{marginBottom: '1rem'}}>
+                        {/* Google Analytics TOS https://marketingplatform.google.com/about/analytics/terms/us/ */}
+                        {/* Text form https://www.termsfeed.com/public/uploads/2019/04/privacy-policy-template.pdf */}
+                        {/* What's in a privacy policy: https://www.nolo.com/legal-encyclopedia/what-to-include-in-your-website-s-privacy-policy.html */}
+                        <b>Sexy Tofu</b> operates <a href="https://www.sexytofu.org/">https://www.sexytofu.org/</a> (the "Site").
+                        This privacy policy informs you of our policies regarding the collection, use and disclosure of
+                        personal information we receive from users of the Site.
+                        We use your personal information only for providing and improving the Site. By using the Site, you
+                        agree to the collection and use of information in accordance with this policy.
+                    </Typography>
+                    <Typography component={'span'} variant="h3" display="block" align="left" style={{color: "#322737", marginBottom: '1rem'}}>
+                        Cookies
+                    </Typography>
+                    <Typography component={'span'} variant="body2" display="block" align="left" style={{marginBottom: '1rem'}}>
+                        Cookies are small files of data, which may include an anonymous unique identifier.
+                        Cookies are sent to your browser from a web site and stored on your computer's hard drive.
+                        Like many sites, we use "cookies" to collect information about your activities
+                        in our services, which may include:
+                        <ul>
+                            <li> Foods you search for</li>
+                            <li> Recommendations you apply</li>
+                            <li> Number of people and length of time your grocery list is for </li>
+                        </ul>
+                        We use cookies from third-party partners, such as&nbsp;
+                        <a href="https://www.google.com/policies/privacy/partners/" target="_blank" rel="noopener noreferrer">Google Analytics</a>,&nbsp;
+                        {/* TODO: disclose ALL information collected! be specific. How do you share info? How do you use it? what do you use? */}
+                        to track how you use this website so we may improve this website in the future.
+                        You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent.
+                        The amount of information provided by you is voluntary.
+                        However, if you do not accept cookies, you might not be able to use some parts of our Site.
+                    </Typography>
+                    <Typography component={'span'} variant="h3" display="block" align="left" style={{color: "#322737", marginBottom: '1rem'}}>
+                        Your Data
+                    </Typography>
+                    <Typography component={'span'} variant="body2" display="block" align="left" style={{marginBottom: '1rem'}}>
+                        Please note <b>Sexy Tofu</b> takes privacy seriously. Data collected is used for internal purposes only.
+                        We do not sell or rent information about you.
+                        We do not share or disclose your personal information and data to
+                        third parties without your consent, except as explained in this Privacy Policy.
+                    </Typography>
+                    <Typography component={'span'} variant="h3" display="block" align="left" style={{color: "#322737", marginBottom: '1rem'}}>
+                        Compliance with Laws
+                    </Typography>
+                    <Typography component={'span'} variant="body2" display="block" align="left" style={{marginBottom: '1rem'}}>
+                        <b>Sexy Tofu</b> cooperates with the law and government officials to comply with the law. We may disclose
+                        information about you if we deem it reasonably necessary to:
+                        <ol type="a">
+                            <li> Satisfy relevant laws or regulations</li>
+                            <li> Enforce Terms of Use, including investigations of potential violations</li>
+                            {/* TODO: a Terms of Use, and link here */}
+                            <li> Detect, prevent, and address fraud, security, or technical issues</li>
+                            <li> Protect the rights, safety, and property of this company, its users, or the public</li>
+                        </ol>
+                        as required or permitted by law.
+                        Your data is subject to the laws of the United States of America regardless of where the data originates.
+                    </Typography>
+                    <Typography component={'span'} variant="h3" display="block" align="left" style={{color: "#322737", marginBottom: '1rem'}}>
+                        Security
+                    </Typography>
+                    <Typography component={'span'} variant="body2" display="block" align="left" style={{marginBottom: '1rem'}}>
+                        The security of your personal information is important to us, but remember that no method of transmission over the internet,
+                        or method of electronic storage, is always completely secure.
+                        While we strive to use commercially acceptable means to protect your Personal Information,
+                        we cannot guarantee its absolute security.
+                    </Typography>
+                    <Typography component={'span'} variant="h3" display="block" align="left" style={{color: "#322737", marginBottom: '1rem'}}>
+                        Changes to this privacy policy
+                    </Typography>
+                    <Typography component={'span'} variant="body2" display="block" align="left" style={{marginBottom: '1rem'}}>
+                        This Privacy Policy is effective as of <b>{LAST_UPDATED_DATE}</b> and will remain in effect
+                        except with respect to any changes in its provisions in the future,
+                        which will be in effect immediately after being posted on this page.
+                        We reserve the right to update or change our Privacy Policy at any time and
+                        you should check this Privacy Policy periodically. Your continued use of the Service
+                        after we post any modifications to the Privacy Policy on this
+                        page will constitute your acknowledgment of the modifications and your consent
+                        to abide and be bound by the modified Privacy Policy.
+                    </Typography>
+                    <Typography component={'span'} variant="h3" display="block" align="left" style={{color: "#322737", marginBottom: '1rem'}}>
+                        Contact us
+                    </Typography>
+                    <Typography component={'span'} variant="body2" display="block" align="left" style={{marginBottom: '1rem'}}>
+                        If you have any questions about this Privacy Policy, please&nbsp;
+                        <a href="mailto: sexytofu.info@gmail.com">contact us</a>.
+                    </Typography>
                 </DialogContent>
           </Dialog>
         )
