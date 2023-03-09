d3297eb6911e2f6683e0b5719622af60ad01081d
Odin <jfhb@qq.com>
2023-03-09 21:25:54 +0800

    update part about pack the extension.
    By Odin

--- web-plugin/packFile.bat
+++ web-plugin/packFile.bat
@@ -1,20 +1,26 @@
-@echo off
+@echo on
 
 setlocal enabledelayedexpansion
 set chromeExe=C:\Program Files\Google\Chrome\Application\chrome.exe
+set Zip=C:\Program Files\7-Zip\
 set tmpFolder=tmp
+
 ::back to up folder
 pushd ..
 echo.
 echo   ©°©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©´
 echo     This tool will help to pack the extension, and pack name as "%tmpFolder%", 
-echo     tmp file in : "./%tmpFolder%"
+echo        tmp file in : "./%tmpFolder%"
+echo     The tool need 7zip for zip the %tmpFolder%,
+echo       Zip folder: %Zip%
 echo     You can define the tmpFolder in the script as: set tmpFolder=[New Name]
 echo   ©¸©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¼
 echo.
 
 if exist %tmpFolder% rd %tmpFolder% /s /q
 xcopy /S /Y .\web-plugin  .\%tmpFolder%\ /Exclude:.\web-plugin\exclude.txt >nul
+
+"%zip%\7z.exe" a  %tmpFolder%.zip  .\%tmpFolder%
 
 echo.
 echo If exist perm file here, will reuse it, or will generate a new file...
